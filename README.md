ae6c172beab86703b3fe46e7626d8ce7f7fb119c
Артур Галеев <a.galeev@tapir.ws>
2020-10-31 14:14:14 +0500

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# MyCalendar
+by Artur
+
+##### Установка
+- `npm install`
+
+##### Запуск локального сервера
+- `npm run dev`
+
+##### Сборка для прода
+- `npm run prod`
---
+++ src/templates/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <my-calendar />
+    </div>
+  </div>
+</template>
+
+<script>
+import MyCalendar from '../components/MyCalendar.vue'
+export default {
+  components: {
+    MyCalendar
+  }
+}
+</script>
